2e4d52e050f5680026a9e1da392232e89c77430e
German Priluckiy <priluckiygk@yandex.ru>
2023-01-11 22:05:52 +0300

    Iteration2

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,20 @@ const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
 const popupContainer = document.querySelector('.popup__container');
-const saveBtn = popupContainer.querySelector('.popup__save-btn');
-const inputName = popupContainer.querySelector('.popup__input-name');
-const inputJob = popupContainer.querySelector('.popup__input-job');
+const inputName = popupContainer.querySelector('#name');
+const inputJob = popupContainer.querySelector('#job');
 const editBtn = profile.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
-const closeBtn = popupContainer.querySelector('.popup__close-btn');
+const closeBtn = popupContainer.querySelector('.profile-edit__close-btn');
 
 function popupMenuOpen() {
-  popup.classList.remove('popup_closed');
+  popup.classList.add('popup_opened');
   inputName.setAttribute('value', profileName.textContent);
   inputJob.setAttribute('value', profileJob.textContent);
 }
 
 function popupMenuClose() {
-  popup.classList.add('popup_closed');
+  popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
