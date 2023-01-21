ab77de6f5fa406bbfb2e8c2727067e10d04b62d8
German Priluckiy <priluckiygk@yandex.ru>
2023-01-21 19:57:27 +0300

    add like function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
@@ -7,17 +34,26 @@ const inputJob = popupContainer.querySelector('#job');
 const editBtn = profile.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const closeBtn = popupContainer.querySelector('.profile-edit__close-btn');
+const likeBtn = document.querySelectorAll('.like-btn');
+const likeArray =  Array.from(likeBtn);
 
+//Открытие редактирования
 function popupMenuOpen() {
   popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
 }
 
+editBtn.addEventListener('click', popupMenuOpen);
+
+//Закрытие редактирования
 function popupMenuClose() {
   popup.classList.remove('popup_opened');
 }
 
+closeBtn.addEventListener('click', popupMenuClose);
+
+//Сохранение профиля
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
@@ -25,8 +61,19 @@ function handleFormSubmit (evt) {
   popupMenuClose();
 }
 
-editBtn.addEventListener('click', popupMenuOpen);
-closeBtn.addEventListener('click', popupMenuClose);
-
 popupContainer.addEventListener('submit', handleFormSubmit);
+
+//Реализация кнопки like
+likeArray.forEach( (item) => {
+  item.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('like-btn_status_active');
+  });
+});
+
+
+
+
+
+
+
 
